47d4807d6fe7f8bc5ccd060e5055ed0c416a9d37
McelikZ <133252909+McelikZ@users.noreply.github.com>
2025-07-07 11:29:20 +0300

    customs,pages,redux
    added customs components
    added pages
    added redux files

---
+++ src/screens/index.js
@@ -0,0 +1,11 @@
+import ForgotPasswordPage from "./ForgotPasswordPage";
+import HomePage from "./HomePage";
+import LoginPage from "./LoginPage";
+import SignUpPage from "./SignUpPage";
+
+export{
+    ForgotPasswordPage,
+    HomePage,
+    LoginPage,
+    SignUpPage,
+}
