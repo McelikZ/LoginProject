d875cf7758934e48bf1f095ae5fe34f44c3818ea
McelikZ <133252909+McelikZ@users.noreply.github.com>
2025-07-20 14:15:23 +0300

    app.js
    changed:app.js

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import RootNavigation from "./src/navigation/RootNavigation"
 import store from './src/redux/store'
 import { Provider } from 'react-redux'
-//import app  from "./firebaseConfig"
+//import app  from "./firebaseConfig" ---you need to import firebaseConfig.js if you want to use Firebase features
 const App = () => {
   return (
     <Provider store={store}>
