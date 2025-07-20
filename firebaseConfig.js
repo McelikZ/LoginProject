1780e61e2860232ae2cf7d3b692f739832d74b5b
McelikZ <133252909+McelikZ@users.noreply.github.com>
2025-07-20 13:16:41 +0300

    app.js,firebaseconfig
    changed:app.js
    changed:firebaseconfig

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -7,14 +7,9 @@ import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCI8x57ePulTTDra82uRYCXPobYKjbmMeQ",
-  authDomain: "loginproject-c176e.firebaseapp.com",
-  projectId: "loginproject-c176e",
-  storageBucket: "loginproject-c176e.firebasestorage.app",
-  messagingSenderId: "299570814041",
-  appId: "1:299570814041:web:c9d8350a9c41e5ebc9a548"
-};
+// const firebaseConfig = {
+
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
