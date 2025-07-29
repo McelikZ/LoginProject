43e87883bab6eaa1f3ec74357738f05c54df5068
Muhammet Çelik <muhammedcelik17@hotmail.com>
2025-07-29 21:43:50 +0300

    firebaseConfig
    Changed:firebaseConfig

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,12 +3,7 @@ import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCI8x57ePulTTDra82uRYCXPobYKjbmMeQ",
-  authDomain: "loginproject-c176e.firebaseapp.com",
-  projectId: "loginproject-c176e",
-  storageBucket: "loginproject-c176e.firebasestorage.app",
-  messagingSenderId: "299570814041",
-  appId: "1:299570814041:web:c9d8350a9c41e5ebc9a548"
+  
 };
 const app = initializeApp(firebaseConfig);
 const auth = initializeAuth(app, {
