b68d3ec26530ea944b767cb2a785116a41faa83d
Muhammet Çelik <133252909+McelikZ@users.noreply.github.com>
2025-07-31 00:59:16 +0300

    Update LoginPage.style.js

--- src/screens/Login/LoginPage.style.js
+++ src/screens/Login/LoginPage.style.js
@@ -1,50 +1,50 @@
-import { StyleSheet, StatusBar, Platform } from "react-native";
-import { colors, spacing, fonts } from "../../theme/index";
+import { StyleSheet,StatusBar,Platform } from "react-native";
+import {colors,spacing,fonts} from "../../theme/index"
 
 export default StyleSheet.create({
-  mainContainer: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-    backgroundColor: "white",
-  },
-  headerContainer: {
+   mainContainer:{
+    flex:1,
+    alignItems:'center',
+    justifyContent:'center', 
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    backgroundColor:'white'
+   },
+   headerContainer:{
+    flex:2,
+    alignItems:'center',
+    justifyContent:'center',
+    marginTop:spacing.medium
+   },
+   contentContainer: {
     flex: 2,
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: 20,
-  },
-  contentContainer: {
-    flex: 2,
-    alignItems: "center",
-    paddingTop: 120,
-  },
-  footerContainer: {
-    flex: 1,
-    alignItems: "center",
-    flexDirection: "row",
-    paddingTop: 100,
-  },
-  bottomTextContainer: {
-    flexDirection: "row",
-    paddingBottom: 50,
-  },
-  signInStyle: {
-    fontWeight: "bold",
-    fontSize: fonts.sizes.xlarge,
-    marginBottom: 20,
-    opacity: 0.8,
-  },
-  enterValidStyle: {
-    opacity: 0.7,
-  },
-  orContinueWithStyle: {
-    margin: 20,
-    opacity: 0.5,
-  },
-  haventAnyAccountStyle: {
-    opacity: 0.8,
-    fontSize: fonts.sizes.smallMedium,
-  },
-});
+    alignItems: 'center',
+    paddingTop:spacing.huge,
+   },
+   footerContainer:{
+    flex:1,
+    alignItems:'center',
+    flexDirection: 'row',
+    paddingTop:spacing.huge,
+   },
+   bottomTextContainer:{
+    flexDirection: 'row',
+    paddingBottom:spacing.medium,
+   },
+   signInStyle:{
+    fontWeight:'bold',
+    fontSize:fonts.sizes.xlarge,
+    marginBottom:spacing.medium,
+    opacity:0.8,
+   },
+   enterValidStyle:{
+     opacity:0.7,
+   },
+   orContinueWithStyle:{
+    margin:spacing.medium,
+    opacity:0.5,
+   },
+   haventAnyAccountStyle:{
+   opacity: 0.8,     
+   fontSize: fonts.sizes.smallMedium,  
+   }
+})
