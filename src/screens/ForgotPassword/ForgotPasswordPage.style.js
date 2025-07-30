7b8ce3e00d7b039ef63150ab379eb40edcb22126
Muhammet Çelik <133252909+McelikZ@users.noreply.github.com>
2025-07-31 00:58:57 +0300

    Update ForgotPasswordPage.style.js

--- src/screens/ForgotPassword/ForgotPasswordPage.style.js
+++ src/screens/ForgotPassword/ForgotPasswordPage.style.js
@@ -1,39 +1,39 @@
-import { StyleSheet, StatusBar, Platform } from "react-native";
-import { colors, spacing, fonts } from "../../theme/index";
+import { StyleSheet,StatusBar,Platform } from "react-native";
+import {colors,spacing,fonts} from "../../theme/index"
 
 export default StyleSheet.create({
-  mainContainer: {
+   mainContainer:{
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-  },
-  headerContainer: {
-    flex: 3,
-    alignItems: "center",
-    justifyContent: "center",
-    paddingBottom: 100,
-  },
-  contentContainer: {
+    alignItems:'center',
+    justifyContent:'center',
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+   },
+   headerContainer:{
+    flex:3,
+    alignItems:'center',
+    justifyContent:'center',
+    paddingBottom:spacing.hugeSmall
+   },
+   contentContainer: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    paddingBottom: 130,
-  },
-  footerContainer: {
-    flex: 2,
-    alignItems: "center",
-    justifyContent: "center",
-    flexDirection: "row",
-    paddingBottom: 10,
-  },
-  forgetPasswordText: {
-    fontSize: 25,
-    fontWeight: "bold",
-    margin: 10,
-  },
-  dontWorryText: {
-    opacity: 0.5,
-    fontSize: 14,
-  },
-});
+    alignItems: 'center',
+    justifyContent:'center',
+    paddingBottom:spacing.hugeSmall
+   },
+   footerContainer:{
+    flex:2,
+    alignItems:'center',
+    justifyContent:'center',
+    flexDirection:'row',
+    paddingBottom:spacing.medium
+   },
+   forgetPasswordText:{
+    fontSize:25,
+    fontWeight:'bold',
+    margin:spacing.medium
+   },
+   dontWorryText:{
+    opacity:0.5,
+    fontSize:fonts.sizes.medium,
+   }
+})
