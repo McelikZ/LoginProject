ba9c938734d14cfe8b9e43afb80f338f8b1cc1ad
Muhammet Çelik <133252909+McelikZ@users.noreply.github.com>
2025-07-31 00:59:47 +0300

    Update SignUpPage.style.js

--- src/screens/SignUp/SignUpPage.style.js
+++ src/screens/SignUp/SignUpPage.style.js
@@ -1,49 +1,51 @@
-import { StyleSheet, StatusBar, Platform } from "react-native";
-import { colors, spacing, fonts } from "../../theme/index";
+import { StyleSheet,StatusBar,Platform } from "react-native";
+import {colors,spacing,fonts} from "../../theme/index"
 
 export default StyleSheet.create({
-  mainContainer: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-  },
-  headerContainer: {
-    flex: 2,
-    alignItems: "center",
-    justifyContent: "center",
-    paddingBottom: 120,
-  },
-  contentContainer: {
-    flex: 2,
-    alignItems: "center",
-    justifyContent: "center",
-    marginBottom: 25,
-  },
-  firstRowContainer: {
-    flexDirection: "row",
-    paddingTop: 10,
-  },
-  secondRowContainer: {
-    flexDirection: "row",
-    paddingTop: 5,
-  },
-  footerContainer: {
-    flex: 1,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-    marginBottom: 50,
-  },
-  signUpStyle: {
-    fontSize: 25,
-    fontWeight: "bold",
-    opacity: 0.7,
-    marginBottom: 10,
-  },
-  infoTextSyle: {
-    fontSize: 15,
-    fontWeight: "bold",
-    opacity: 0.5,
-  },
-});
+    mainContainer:{
+     flex:1,
+     alignItems:'center',
+     justifyContent:'center',
+     paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    },
+    headerContainer:{
+     flex:2,
+     alignItems:'center',
+     justifyContent:'center',
+     paddingBottom:spacing.huge,
+     
+    },
+    contentContainer:{
+     flex:2,
+     alignItems:'center',
+     justifyContent:'center',
+     marginBottom:spacing.mediumLarge,
+    },
+    firstRowContainer:{
+     flexDirection: 'row',
+     paddingTop:spacing.small,
+    },
+    secondRowContainer:{
+     flexDirection: 'row',
+     paddingTop:spacing.tiny
+    },
+    footerContainer:{
+     flex:1,
+     flexDirection:'row',
+     alignItems:'center',
+     justifyContent:'center',
+     marginBottom:spacing.xlarge
+    },
+    signUpStyle:{
+     fontSize:fonts.sizes.large,
+     fontWeight:'bold',
+     opacity:0.7,
+     marginBottom:spacing.small,
+    },
+    infoTextSyle:{
+     fontSize:fonts.sizes.medium,
+     fontWeight:'bold',
+     opacity:0.5
+    },
+    
+})
